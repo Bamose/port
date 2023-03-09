ed9d607c1aa3728669fd7f8712577806fcb619af
Anella N <anellanegash@gmail.com>
2023-03-09 21:42:30 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:[" ","FrontEnd Developer","Web developer" ],
+    strings:[" ","FullStack Developer","Web developer" ],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
